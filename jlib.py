014e0a3e96bf8aa74e1ee29448ac6333fb052125
nande <devnull@localhost>
2010-09-14 05:08:21 +0000

    todos los tipos de orden!!

--- jlib.py
+++ jlib.py
@@ -61,14 +61,13 @@ procs = [
 def a2proc(procarr, num = 0):
 	#por un array devuelve un proceso
 	class p:
-		ini = 0
-		fin = 0
-		prio = 0
-		cpu = 0
-		num = 0
+		pass
+		
 	tp = p()
 	tp.cpu, tp.prio, tp.ini = procarr
 	tp.num = num
+	tp.q = 1
+	tp.fin = 0
 	return tp
 
 def aa2aproc(arr):
@@ -81,61 +80,102 @@ def aa2aproc(arr):
 	#los ordenamos
 	a.sort(key=getini )
 	return a
+	
+class Plan:
+	p = None
+	def __init__(self,  procs,  q,  changeprio=False):
+		self.procs = aa2aproc(procs)
+		self.act = []
+		self.terminados = []
+		self.time=0
+		self.q = q
+		self.cp = changeprio
+		
+	def activos(self):
+		#devuelve los procesos activos, para RRobbin
+		#procs ha de ser un array de procesos (como en aa2aproc)
+		#p = el proceso que se estaba ejecutando
+		for i in self.procs: 
+			if i.ini <self.time :
+				self.act.append(i)
+		if self.p:
+			if self.cp :
+				self.p.prio+=1
+				self.p.q *=2
+			self.act.append(self.p)
+		for i in self.procs :
+			if i.ini == self.time:
+				self.act.append(i)
+		for i in self.act:
+			if i in self.procs:
+				self.procs.remove(i)
+				
+	def BasicLap(self):
+		if self.act:
+			#no lo guardamos en self.p para que activos no lo meta d nuevo
+				p = self.act[0]
+				self.time += p.cpu
+				p.fin = self.time
+				self.terminados.append(p)
+				self.act.remove(p)
+		self.activos()
+		if not self.act : self.time += self.q
+		
+	def RRLap(self,  sort=False):
+		if self.act:
+			self.p = self.act[0]
+			q = self.q*self.p.q
+			self.p.cpu -= q
+			self.time += (q-self.q)
+			self.act.remove(self.p)
+			if self.p.cpu <= 0:
+				self.p.fin = self.time
+				self.terminados.append(self.p)
+				self.p = None
+		self.activos()		
+		self.time += self.q
+		
+def Fifo(procs,  q=5):
+	plan = Plan(procs,  q)
+	while plan.procs or plan.act:
+		plan.BasicLap()
+	return plan.terminados
 
-def activos(procs, t):
-	#devuelve los procesos activos,
-	#procs ha de ser un array de procesos (como en aa2aproc)
-	act = []
-	for i in procs:
-		if i.ini <=t :
-			act.append(i)
-	for i in act:
-		procs.remove(i)
-	return act
-
-def Fifo(procs, q=5,  pmc=False):
-	#First in first out
-	#pmc=Primero el Mas corto
+def MasCorto(procs,  q=5):
 	def getcpu(p): return p.cpu
-	procs = aa2aproc(procs)
-	time = 0
-	act = []
-	terminados = []
-	while procs or act:
-		if pmc:
-			act.sort(key=getcpu)
-		if act:
-				p = act[0]
-				time += p.cpu
-				p.fin = time
-				terminados.append(p)
-				act.remove(p)
-		act.extend(activos(procs, time))
-		if not act : time +=q
-	return terminados
-  
-def CalcularTiempos(procs, q = 5, RR=False):
-	#RR=roundrobin-> True=RoundRobin, False=FIFO
-  procs = aa2aproc(procs)
-  time = 0
-  act = []
-  terminados = []
-  p=None
-  while procs or act:
-	if act:
-		p = act[0]
-		p.cpu -= q
-		time+=q
-		if p.cpu <=0:
-			p.fin = time
-			terminados.append(p)
-			act.remove(p)
-			p=None #para el if de abajo
-		elif RR:
-			act.remove(p)
-	act.extend(activos(procs, time))
-	if RR and p: act.append(p)
-  return terminados
-  
-for i in CalcularTiempos(procs,  5,  True):
-    print i.num,  i.fin
+	plan = Plan(procs,  q)
+	while plan.procs or plan.act:
+		plan.BasicLap()
+		plan.act.sort(key=getcpu)
+	return plan.terminados
+		
+def RRobbin(procs,  q=5):
+	plan = Plan(procs,  q)
+	while plan.procs or plan.act:
+		plan.RRLap()
+	return plan.terminados
+	
+def Prio(procs,  q=5,  mcolas = False):
+	plan = Plan(procs,  q,  mcolas)
+	def getprio(p): return p.prio
+	while plan.procs or plan.act:
+		plan.RRLap()
+		plan.act.sort(key=getprio)
+	return plan.terminados
+
+def MColas(procs,  q=5):
+	return Prio(procs,  q,  mcolas=True)
+	
+"""for i in Fifo(procs):	print i.num,  i.fin
+print ""
+
+for i in RRobbin(procs):	print i.num,  i.fin
+print ""
+
+for i in MasCorto(procs): print i.num,  i.fin
+print ""
+
+for i in Prio(procs): print i.num ,  i.fin
+print ""
+
+for i in MColas(procs):    print i.num,  i.fin"""
