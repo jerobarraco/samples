ff2b60674a3c337b0d894ea404ec83caa9d14b4a
jerobarraco <devnull@localhost>
2014-08-19 13:08:32 -0300

    detalles

--- python/qt/scene.py
+++ python/qt/scene.py
@@ -4,6 +4,8 @@ import PySide
 from PySide.QtGui import QGraphicsScene, QGraphicsView, QApplication, QPen, QBrush, QPixmap, QPainter
 # http://qt-project.org/doc/qt-4.8/graphicsview.html
 # http://srinikom.github.io/pyside-docs/PySide/QtGui/QGraphicsView.html
+# https://deptinfo-ensip.univ-poitiers.fr/ENS/pyside-docs/PySide/QtGui/QGraphicsView.html?highlight=qgraphicsview#PySide.QtGui.QGraphicsView
+# https://deptinfo-ensip.univ-poitiers.fr/ENS/pyside-docs/PySide/QtGui/QGraphicsScene.html?highlight=qgraphicsscene#PySide.QtGui.QGraphicsScene
 # https://deptinfo-ensip.univ-poitiers.fr/ENS/pyside-docs/PySide/QtGui/QPen.html?highlight=qpen#PySide.QtGui.QPen
 # https://deptinfo-ensip.univ-poitiers.fr/ENS/pyside-docs/PySide/QtGui/QBrush.html?highlight=qbrush#PySide.QtGui.QBrush
 # https://deptinfo-ensip.univ-poitiers.fr/ENS/pyside-docs/PySide/QtGui/QPixmap.html?highlight=qpixmap#PySide.QtGui.QPixmap
@@ -23,13 +25,14 @@ def grabar():
 	# http://developer.nokia.com/community/wiki/QPainter::begin:Paint_device_returned_engine_%3D%3D_0_(Known_Issue)
 	# The solution is pretty simple and it's about defining a valid size for the QPixmap to paint. For instance defining it as QPixmap pix(100, 100) fix this issue.
 	#es imprescindible poner widht y height
+	#crea un pixmap (algo asi como una imagen en memoria)
 	pixmap = QPixmap(scene.width(), scene.height())
 	
-	painter = QPainter(pixmap)
+	painter = QPainter(pixmap)#crea un painter sobre ese pixmap (que es el que pasara lo del scene al pixmap)
 	painter.setRenderHint(QPainter.Antialiasing)#le dice que use Antialiasing
 	scene.render(painter)#le dice a la escena que se dibuje sobre el painter
 	painter.end()#termina el painter
-	pixmap.save(str(CUENTA)+".png")#lo guarda
+	pixmap.save(str(CUENTA)+".png")#lo guarda, usa cuenta que va contando de 0, 1, 2 etc
 	CUENTA += 1
 
 class View(QGraphicsView):#creo una clase solo para poder implementar el keyPressEvent
@@ -40,15 +43,16 @@ class View(QGraphicsView):#creo una clase solo para poder implementar el keyPres
 			grabar()
 			
 #crea la app, la ventana y el view y la escena
-app = QApplication(sys.argv)#si no creara la clase App con esto bastaria
+app = QApplication(sys.argv)
 scene = QGraphicsScene()
-#view = QGraphicsView(scene)
+#view = QGraphicsView(scene) #ni no usara la clase View con esto basta
 view = View(scene)
 view.show()
 
 
 #dibuja algo en la escena
-scene.addRect(QRectF(0, 0, 100, 200), QPen(Qt.black), QBrush(Qt.green));
+#rectangulo de 100x200 en 0,0 borde negro relleno verde
+scene.addRect(QRectF(0, 0, 100, 200), QPen(Qt.black), QBrush(Qt.green))
 
 #ejecuta la app 
 app.exec_()
