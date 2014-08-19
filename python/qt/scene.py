13139669a72b86a62ad3350c3a51029109922347
jerobarraco <devnull@localhost>
2014-08-19 12:58:46 -0300

    bug del tamaño

--- python/qt/scene.py
+++ python/qt/scene.py
@@ -14,13 +14,16 @@ from PySide.QtCore import QRectF, Qt
 # https://deptinfo-ensip.univ-poitiers.fr/ENS/pyside-docs/PySide/QtCore/QRectF.html#PySide.QtCore.QRectF
 
 CUENTA = 0
-def grabar(w, h):
+def grabar():
 	#Defino una funcion para grabar porque la app tiene que haberse ejecutado para poder grabarlo creo
+	# ojo que esto es horrendo, estoy usando el scene como "global" y el cuenta tambien
+	
 	#lo graba/imprime
-	global CUENTA
+	global CUENTA, scene#toma estas variables desde afuera de esta funcion, esto se considera feo,
 	# http://developer.nokia.com/community/wiki/QPainter::begin:Paint_device_returned_engine_%3D%3D_0_(Known_Issue)
 	# The solution is pretty simple and it's about defining a valid size for the QPixmap to paint. For instance defining it as QPixmap pix(100, 100) fix this issue.
-	pixmap = QPixmap(w, h)
+	#es imprescindible poner widht y height
+	pixmap = QPixmap(scene.width(), scene.height())
 	
 	painter = QPainter(pixmap)
 	painter.setRenderHint(QPainter.Antialiasing)#le dice que use Antialiasing
@@ -34,8 +37,7 @@ class View(QGraphicsView):#creo una clase solo para poder implementar el keyPres
 		print("key")
 		if event.key() == Qt.Key_P:
 			print("lololol")
-			tam = self.size()#tomamos el tamanyo del view (porque la ventana puede cambiar el tamanyo)
-			grabar(tam.width(), tam.height())
+			grabar()
 			
 #crea la app, la ventana y el view y la escena
 app = QApplication(sys.argv)#si no creara la clase App con esto bastaria
