eb5ef76ac2dc932de4876c16e6e58d768f66c0d2
jerobarraco <devnull@localhost>
2014-11-21 14:51:37 -0300

    contar par e impares

--- asm/mayormenor.asm
+++ asm/mayormenor.asm
@@ -1,38 +1,52 @@
-
-; You may customize this and other start-up templates; 
-; The location of this template is c:\emu8086\inc\0_com_template.txt
-
-
 org 100h
 .data
-nums DB "ojsdlfkjd{SAFÑWER_:;ZXCVW$%&][*¨1234567890'¿"
+; como no puedo poner cualquier direccion uso las variables
+; son casi lo mismo
+; datos falsos
+nums DB "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!$%&/()=?¿-_<>#,;.:*º\{}[]"
+; la direccion de nums en tu ejercicio seria 50
+; la de pares 600, e impares 601
 pares DB 0
 impares DB 0  
 
 .code           
-; datos falsos
-; segun el ejercicio deberia ser mov ax, 500
-; en dx guardamos la DIRECCION de los datos,
+; segun el ejercicio deberia ser mov bx, 500
+; en bx guardamos la DIRECCION de los datos,
 ; o sea, la direccion del PRIMER elemento
-mov dx, @data
-add dx, 2
+lea bx, [nums]
 
 ;inicializamos el contador; int i = 100
 mov cx, 100 
 ;hacemos todo
 L:;por cada elemento 
     ; colocamos el elemento en ax para trabajar
-    ; tomamos el elemento en la direccion que apunta dx
-    mov ax, [dl]   
+    ; tomamos el elemento en la direccion que APUNTA bx
+    ; bx tendria la DIRECCION de cada elemento
+    mov al, [bx]
     ; "dividimos" por dos (lo que nos interesa es el resto)
     ; al hacer un shift, como la base es 2, estamos dividiendo por dos
+    ; el bit que sobra se mete en CF (carry flag)
+    ; si ese bit es 1, el numero era impar
     shr ax, 1
-    
+	
+	; si el carry es 1 entonces era impar, y saltamos ahi
+	; if (ax%2==0){}
+    JC impar; 
+    ; Si el carry era 0 se ejecuta esto. e sea, par
+    PAR:        
+    	; en el ejercicio seria inc [601]
+       inc pares                  
+       ; si era par tenemos que saltearnos la parte impar
+       ; lo que seria saltar hasta el "FinSi"
+       jmp TERM 
+    IMPAR:; else{}
+		inc impares    
+    TERM:
     ; pasamos al siguiente elemento
     ; como dx tiene la direccion, al incrementarlo nos queda
     ; apuntando al siguiente elemento (siempre que el elemento sean bytes)
-    inc dx
-loop L;decrementa CX _Y_ salta a el label (ini) si CX NO es zero
+    inc bx
+loop L;decrementa CX _Y_ salta a el label (L) si CX NO es zero
 ;dec cx;decrementar (i--)
 ;jnz ini; while (i!=0)
 
@@ -41,3 +55,5 @@ loop L;decrementa CX _Y_ salta a el label (ini) si CX NO es zero
 
 
 ret
+
+
