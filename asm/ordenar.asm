f65d5f683fbffbd0436961637e9c76f456425e15
jerobarraco <devnull@localhost>
2014-12-04 23:24:20 -0300

    de nuevo en 100

--- asm/ordenar.asm
+++ asm/ordenar.asm
@@ -8,24 +8,23 @@ datos DB "asffdrujkñsalfmxcvpwoeirkdjfKMQWN ROISDMVKLAÑDURQIW$%&/()=?¿-_<>#,;.:*
 
 .code
 
-MOV DX, 0 ; la variable contador I
-
-LA: ; para i = 0 hasta 100
+MOV DX, 0 ; la variable contador I 
+LA: ; para I = 0 hasta 100
     LEA SI, [datos]
-    MOV CX, 50
-    SUB CX, DX ; para J = 0 hasta 100-i
+    MOV CX, 100 ; inicializamos el contador en 100 elementos (J)
+    SUB CX, DX ; para J = 0 hasta 100-i 
     LB:     
-        MOV AH, [SI]
+        MOV AH, [SI] ; cargamos dos valores consecutivos
         MOV AL, [SI+1]
-        CMP AH, AL
-        JLE FINSI
-            MOV [SI], AL
+        CMP AH, AL ;y los comparamos
+        JLE FINSI; si el primero es MENOR O IGUAL saltamos
+            MOV [SI], AL; sino (si es mayor al otro) los intercambiamos
             MOV [SI+1], AH
         FINSI:
-        INC SI
+        INC SI ; siguiente dato
     LOOP LB; este funciona con el CX
-    INC DX
-    CMP DX, 50
+    INC DX ; el loop trabaja con CX, emulamos un loop con DX, primero lo incrementamos (este va de 0 a 100)
+    CMP DX, 100 ; despues lo comparamos con el máximo (100)
 JL LA ;finpara
 
 ret
