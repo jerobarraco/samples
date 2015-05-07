588f936b9654c93e3681cc1e4f91f742c001127a
jerobarraco <devnull@localhost>
2015-05-07 19:48:04 -0300

    backup polished

--- console/backup.bat
+++ console/backup.bat
@@ -1,11 +1,11 @@
 ﻿@ECHO OFF
 rem Tomado de acá https://gist.github.com/adamcaudill/2322221
 rem colocá esto en algún lugar, y podés configurar windows para que lo ejecute solo http://windows.microsoft.com/en-au/windows/schedule-task#1TC=windows-7
-rem si queres que no se frene y se cierre cuando termine quitale la ultima linea que dice pauses
+rem si queres que no se frene y se cierre cuando termine quitale la ultima linea que dice PAUSE
 rem en caso de necesitar modificar algo, estas 3 cosas siguientes son las mas importantes
 SET CARPETA=D:\mis backups\db\
-set ORIGEN="D:\documentos\Clases\*"
-SET AppExePath="c:\Archivos de Programa\7-Zip\7z.exe"
+set ORIGEN=D:\documentos\Clases\*
+SET AppExePath=c:\Archivos de Programa\7-Zip\7z.exe
 
 set year=%date:~-4,4%
 set month=%date:~-10,2%
@@ -19,32 +19,34 @@ rem set month=%date:~0,2%
 rem set day=%date:~3,2%
 rem set hour=%time:~0,2%
 rem set hour=%hour: =0%
-rem set min=%time:~3,2%
+rem set min=%time:~3,2% 
 rem set pf=%time:~-4,3%
 
-echo hoy es %TODAY% fecha %date% hora %time%
 set ARCHIVO=%year%-%month%-%day%-%hour%%min%.zip
 SET DESTINO="%CARPETA%%ARCHIVO%"
+SET ORIGEN="%ORIGEN%"
+SET AppExePath="%AppExePath%"
 
 if not exist %AppExePath% set AppExePath="%ProgramFiles(x86)%\7-Zip\7z.exe"
 if not exist %AppExePath% set AppExePath="%ProgramFiles%\7-Zip\7z.exe"
 if not exist %AppExePath% goto notInstalled
 
-echo Backing up %ORIGEN% to %DESTINO%
+
 >>registro.log (
-	echo -----------------------NUEVO BACKUP-------------------------------
-	echo fecha %date% hora %time%
+	echo -------------------NUEVO BACKUP-------------------------------
+	echo ------ Fecha %date% -- Hora %time%  -------------
+	echo Haciendo backup de %ORIGEN% a %DESTINO%
 	echo origen %ORIGEN%
 	echo destino %DESTINO%
 	%AppExePath% a -r -tzip -mmt %DESTINO% %ORIGEN%
+	echo Backup de %ORIGEN% a %DESTINO% ha finalizado!
 )
-echo %ORIGEN% backed up to %DESTINO% is complete!
  
 goto end
  
 :notInstalled
  
-echo Can not find 7-Zip, please install it from:
+echo No se encontró 7-Zip, instalelo desde:
 echo  http://7-zip.org/
  
 :end
