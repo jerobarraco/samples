232497faf2aec91386d292060b4609d32d6d146f
jerobarraco <devnull@localhost>
2016-10-18 23:34:25 -0300

    improved

--- python/emptyfolders.py
+++ python/emptyfolders.py
@@ -1,24 +1,34 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import os, sys
 
 if len(sys.argv) >1:
 	mydir = sys.argv[1]
 else:
-	mydir = os.getcwdu()
-print mydir
+	mydir = os.getcwd()
+print (mydir)
 
-borrar = raw_input('Borrar directorios? [y/N]').lower()=='y'
+borrar = input('Borrar directorios? [y/N]').lower()=='y'
+borrar_file = input('Borrar ARCHIVOS? [y/N]').lower()=='y'
 
 for path, dirs, files in os.walk(mydir):
+	
+	for f in files[:]:
+		ext = os.path.splitext(f)[1].lower()
+		if ext not in [ '.mp3', '.ogg', '.aac', '.ac3' ]: 
+			if borrar_file:
+				print ("Borrado arch: " +path+'/'+f)
+				os.remove (path+'/'+f)
+				files.remove(f)
+			else:
+				print ('No borrado arch: '+path+'/'+f)
+				#print (path+'/'+f)
+	
 	if not (files or dirs): 
 		if borrar : 
-			print "Borrado: " +path
+			print ("Borrado: " +path)
 			os.rmdir (path)
 		else:
-			print 'No borrado '+path
+			print ('No borrado: '+path)
 			
-	for f in files:
-		ext = os.path.splitext(f)[1].lower()
-		if ext not in [ '.mp3' ]: print path+'/'+f
 
